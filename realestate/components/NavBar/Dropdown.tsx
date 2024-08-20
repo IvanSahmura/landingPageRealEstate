a0187d88aecf3aee5f63633182aebc8109096651
IvanSahmura <ivansahmura123@gmail.com>
2024-08-20 23:10:34 +0700

    minor fix

--- realestate/components/NavBar/Dropdown.tsx
+++ realestate/components/NavBar/Dropdown.tsx
@@ -9,7 +9,7 @@ interface DropDownProps{
 const Dropdown: React.FC<DropDownProps> = ({isOpen}) => {
 
   return (
-      <CSSTransition in={isOpen} timeout={200} classNames="slide  " unmountOnExit>        
+      <CSSTransition in={isOpen} timeout={200} classNames="slide" unmountOnExit>        
         <ul className="absolute text-xs mt-2 bg-white shadow-lg p-5 space-y-5">
             <li><a href="http://">PROPERTY TYPE</a></li>
             <li><a href="http://">PRICE RANGE</a></li>
